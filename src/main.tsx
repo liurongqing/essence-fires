014bbddffced4a2b8f193be6a8f587cac6cd4ffa
liurongqing <phpgege@xsyx.com>
2021-07-16 17:08:58 +0800

    1

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react'
 import { render } from 'react-dom'
+import { Button } from 'antd'
 
-let Greet = () => <h1>Hello, world123</h1>
+import './aa.css'
+import 'antd/dist/antd.css'
+
+let Greet = () => <>
+		<Button type="primary">123</Button>
+	</>
 render(<Greet />, document.querySelector('#root'))
 
